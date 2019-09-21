a1d862aaf4eb1d2811e0de6e8157eba5704de972
spiny_beast <spiny_beast@mail.ru>
2019-09-21 19:06:15 +0300

    borders

--- src/components/ImageCard/ImageCard.js
+++ src/components/ImageCard/ImageCard.js
@@ -1,13 +1,12 @@
-import React, { useState } from 'react';
-import { IMAGE_URL } from '../../constants';
-import { CategoryTags } from './CategoryTags';
-import { CategoryForm } from './CategoryForm';
-import {Button, Popover, Tooltip} from 'antd';
+import React, {useState} from 'react';
+import {IMAGE_URL} from '../../constants';
+import {CategoryTags} from './CategoryTags';
+import {CategoryForm} from './CategoryForm';
+import {Button, Tooltip} from 'antd';
 
 export default function ImageCard({imageObj, categories, secondGroup = false, getCategories}) {
     const {id, image} = imageObj;
     const [forms, setForms] = useState([]);
-    const [visible, setVisible] = useState(false);
 
     function handleChange(i, event) {
         const values = [...forms];
@@ -17,7 +16,7 @@ export default function ImageCard({imageObj, categories, secondGroup = false, ge
 
     function handleAdd() {
         const values = [...forms];
-        values.push({ value: null });
+        values.push({value: null});
         setForms(values);
     }
 
@@ -26,34 +25,38 @@ export default function ImageCard({imageObj, categories, secondGroup = false, ge
         values.splice(i, 1);
         setForms(values);
     }
-    const content = <div>
-        {
-            categories.map((categoryObj, index) => <CategoryTags key={`category-tags-${index}-${categoryObj.id}`}
-                                                                 categoryObj={categoryObj}
-                                                                 imageObj={imageObj}
-                                                                 getCategories={getCategories}/>)
-        }
-        <Button type={'link'} className="text-primary p-0" href="#" onClick={handleAdd}>Add category</Button>
-        {
-            forms.map((form, idx) =>
-                <CategoryForm
-                    key={idx}
-                    value={form.value || ''}
-                    onChange={e => handleChange(idx, e)}
-                    onRemove={() => handleRemove(idx)}
-                    getCategories={getCategories}
-                />)
-        }
-    </div>;
+
     return (
         <div className={secondGroup ? 'col-3' : 'col-2'}>
-            <Popover content={content} trigger="click"
-                     visible={visible}
-                     onVisibleChange={(open) => setVisible(open)}>
-                <Tooltip title={image.annotation}>
-                    <img key={id} className="img-fluid img-thumbnail w-100" src={IMAGE_URL + image.image} alt={image.source_file}/>
-                </Tooltip>
-            </Popover>
+            <div className="card">
+                <div className="card-body p-0">
+                    <Tooltip title={image.annotation}>
+                        <img key={id} className="img-fluid img-thumbnail w-100" src={IMAGE_URL + image.image}
+                             alt={image.source_file}/>
+                    </Tooltip>
+                    <div className="card-footer">
+                        {
+                            categories.map((categoryObj, index) => <CategoryTags
+                                key={`category-tags-${index}-${categoryObj.id}`}
+                                categoryObj={categoryObj}
+                                imageObj={imageObj}
+                                getCategories={getCategories}/>)
+                        }
+                        <Button type={'link'} className="text-primary p-0" href="#" onClick={handleAdd}>Add
+                            category</Button>
+                        {
+                            forms.map((form, idx) =>
+                                <CategoryForm
+                                    key={idx}
+                                    value={form.value || ''}
+                                    onChange={e => handleChange(idx, e)}
+                                    onRemove={() => handleRemove(idx)}
+                                    getCategories={getCategories}
+                                />)
+                        }
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
